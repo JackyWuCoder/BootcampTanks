ebdab8ffb1d6c95db12c090ff7dcef2328f9a307
JackyWuCoder <JackyWuCoder@gmail.com>
2024-03-25 17:23:54 -0700

    Created a PlayerMovement script
    1) This script inherits from NetworkBehaviour instead of MonoBehaviour.
    2) W and S keys are for forward and backwards movements.
    3) A and D keys are for rotational movements.
    4) Uses the Tank Rigidbody.velocity for movements.

---
+++ Assets/Multiplayer Games Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class PlayerMovement : NetworkBehaviour
+{
+    [SerializeField] private float tankSpeed = 10.0f;
+    [SerializeField] private float tankTurnSpeed = 10.0f;
+
+    private Rigidbody tankRb;
+    private float horizontal;
+    private float vertical;
+
+    public override void OnNetworkSpawn()
+    {
+        base.OnNetworkSpawn();
+        tankRb = GetComponent<Rigidbody>();
+    }
+
+    private void Awake()
+    {
+        tankRb = GetComponent<Rigidbody>();
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (!IsOwner) return;
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        if (!IsOwner) return;
+        tankRb.velocity = tankRb.transform.forward * tankSpeed * vertical;
+        tankRb.rotation = Quaternion.Euler(transform.eulerAngles + (transform.up * tankTurnSpeed * horizontal));
+    }
+}
