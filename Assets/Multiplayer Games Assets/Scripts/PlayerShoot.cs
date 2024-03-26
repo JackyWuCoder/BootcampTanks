35ae1f695b8e07761b5eab3090663405c8d6ffae
JackyWuCoder <JackyWuCoder@gmail.com>
2024-03-25 18:18:31 -0700

    Created PlayerShoot script
    1) PlayerShoot inherits NetworkBehaviour.
    2) We Instantiate the bullet prefab at a shootPoint position, rotation on the current Client.
    3 We then Spawned this bullet across other Clients.
    4) Every time we click the shoot button, we add a force to the bullet's rigidBody that takes in the velocity of the tank added to the bullet's forward shooting speed velocity.
    5) We then destroy this bullet after 5 seconds.

---
+++ Assets/Multiplayer Games Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class PlayerShoot : NetworkBehaviour
+{
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private Transform shootPoint;
+    [SerializeField] private float shootSpeed = 5.0f;
+
+    private Rigidbody tankRb;
+
+    public override void OnNetworkSpawn()
+    {
+        tankRb = GetComponent<Rigidbody>();
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (!IsOwner) return;
+        if (Input.GetButtonDown("Jump"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        GameObject bulletObject = Instantiate(bullet, shootPoint.position, shootPoint.rotation);
+        bulletObject.GetComponent<NetworkObject>().Spawn();
+        bulletObject.GetComponent<Rigidbody>().AddForce(tankRb.velocity + (bulletObject.transform.forward * shootSpeed), ForceMode.VelocityChange);
+        Destroy(bulletObject, 5.0f);
+    }
+}
