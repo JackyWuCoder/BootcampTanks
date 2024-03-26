9bef76541d7e35dd6072367c6d4a4c469c0cd78c
JackyWuCoder <JackyWuCoder@gmail.com>
2024-03-26 16:18:55 -0700

    v2 Build
    1) Allowed Players to set a nickname and a text to display when the Players have joined.
    2) Set the nickname Text UI to be inactive after the Players have joined the game with their chosen nicknames.

--- Assets/Multiplayer Games Assets/Scripts/GameNetworkManager.cs
+++ Assets/Multiplayer Games Assets/Scripts/GameNetworkManager.cs
@@ -4,6 +4,7 @@ using TMPro;
 using Unity.Netcode;
 using Unity.Netcode.Transports.UTP;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameNetworkManager : MonoBehaviour
 {
@@ -11,24 +12,33 @@ public class GameNetworkManager : MonoBehaviour
     [SerializeField] private TMP_InputField ipAddress;
     [SerializeField] private UnityTransport transport;
 
+    [SerializeField] private TMP_InputField playerNameInput;
+    [SerializeField] private Button hostBtn;
+    [SerializeField] private Button clientBtn;
+
     public void JoinHost() 
     {
         NetworkManager.Singleton.StartHost();
-        joinStatusTxt.SetText("Joined as Host");
+        joinStatusTxt.SetText(playerNameInput.text + " joined the game as a Host");
+        playerNameInput.gameObject.SetActive(false);
+
     }
 
     public void JoinClient()
     {
         //transport.ConnectionData.Address = ipAddress.text.Replace(" ", "");
         NetworkManager.Singleton.StartClient();
-        joinStatusTxt.SetText("Joined as Client");
+        joinStatusTxt.SetText(playerNameInput.text + " joined the game as a Client");
+        playerNameInput.gameObject.SetActive(false);
     }
 
+    /*
     public void JoinServer()
     {
         NetworkManager.Singleton.StartServer();
-        joinStatusTxt.SetText("Joined as Server");
+        joinStatusTxt.SetText(playerName.text + " joined the game as a Server");
     }
+    */
 
     // Start is called before the first frame update
     private void Start()
