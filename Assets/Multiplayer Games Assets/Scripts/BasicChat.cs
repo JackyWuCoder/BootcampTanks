c357814020bc7ec464fa6be488ca7a57059712c2
JackyWuCoder <JackyWuCoder@gmail.com>
2024-03-27 23:26:57 -0700

    Basic Chat Setup

--- Assets/Multiplayer Games Assets/Scripts/BasicChat.cs
+++ Assets/Multiplayer Games Assets/Scripts/BasicChat.cs
@@ -1,9 +1,45 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.Netcode;
+using UnityEditor.PackageManager;
 using UnityEngine;
 
-public class BasicChat : MonoBehaviour
+public class BasicChat : NetworkBehaviour
 {
+    [SerializeField] private TMP_InputField chatInput;
+    [SerializeField] private TMP_Text chatText;
+
+    //TO DO: Instead of just posting on the server, update everyone
+    //TO DO: Let each chat show up in a new line
+    public void SendChat()
+    {
+        if (IsServer)
+        {
+            //Call Client RPC
+            ChatClientRPC(NetworkManager.Singleton.LocalClientId + ": " + chatInput.text);
+        }
+        else if (IsClient)
+        {
+            //Call Server RPC
+            ChatServerRPC(NetworkManager.Singleton.LocalClientId + ": " + chatInput.text);
+        }
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    public void ChatServerRPC(string message)
+    {
+        chatText.text = "A client says hi";
+    }
+
+    [ClientRpc]
+    public void ChatClientRPC(string message)
+    {
+        chatText.text = "Server says hi";
+    }
+
+    /*
     // Start is called before the first frame update
     private void Start()
     {
@@ -15,4 +51,5 @@ public class BasicChat : MonoBehaviour
     {
         
     }
+    */
 }
