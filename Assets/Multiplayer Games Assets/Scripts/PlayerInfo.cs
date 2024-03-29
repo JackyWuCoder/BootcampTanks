1931482905d1eec2820bacd19b78ba7cb9e1d7f2
JackyWuCoder <JackyWuCoder@gmail.com>
2024-03-28 19:33:57 -0700

    NetworkVariable for playerName

--- Assets/Multiplayer Games Assets/Scripts/PlayerInfo.cs
+++ Assets/Multiplayer Games Assets/Scripts/PlayerInfo.cs
@@ -1,9 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.Collections;
+using Unity.Netcode;
 using UnityEngine;
 
-public class PlayerInfo : MonoBehaviour
+public class PlayerInfo : NetworkBehaviour
 {
+    [SerializeField] private TMP_Text playerNameTxt;
+
+    // Variable to hold the name of the player
+
+    public NetworkVariable<FixedString64Bytes> playerName = new NetworkVariable<FixedString64Bytes>(
+        new FixedString64Bytes("Player name"),
+        NetworkVariableReadPermission.Everyone,
+        NetworkVariableWritePermission.Owner);
+
+    public override void OnNetworkSpawn()
+    {
+        playerName.OnValueChanged += OnNameChanged;
+    }
+
+    private void OnNameChanged(FixedString64Bytes preVal, FixedString64Bytes newVal)
+    {
+        if (newVal != preVal)
+        {
+            playerNameTxt.SetText(newVal.Value);
+        }
+    }
+
+    public void SetName(string name)
+    {
+        playerName.Value = new FixedString64Bytes(name);
+    }
+
+    public override void OnNetworkDespawn()
+    {
+        playerName.OnValueChanged -= OnNameChanged;
+    }
 
     /*
     // Start is called before the first frame update
