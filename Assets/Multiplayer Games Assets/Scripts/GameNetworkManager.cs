3ae3542216333587ba11a48a0825b2ea120d5316
JackyWuCoder <JackyWuCoder@gmail.com>
2024-05-20 03:25:23 -0700

    Tank Color Change
    1) Created Dropdown UI and a Button to Submit.

--- Assets/Multiplayer Games Assets/Scripts/GameNetworkManager.cs
+++ Assets/Multiplayer Games Assets/Scripts/GameNetworkManager.cs
@@ -10,20 +10,19 @@ public class GameNetworkManager : MonoBehaviour
     [SerializeField] private TMP_Text joinStatusTxt;
     [SerializeField] private TMP_InputField ipAddress;
     [SerializeField] private UnityTransport transport;
-
     [SerializeField] private TMP_InputField playerNameInput;
 
     public void JoinHost()
     {
         NetworkManager.Singleton.StartHost();
-        joinStatusTxt.SetText(playerNameInput.text + " joined the game as a Host");
+        joinStatusTxt.SetText($"{playerNameInput.text} joined the game as a Host");
     }
 
     public void JoinClient()
     {
         //transport.ConnectionData.Address = ipAddress.text.Replace(" ","");
         NetworkManager.Singleton.StartClient();
-        joinStatusTxt.SetText(playerNameInput.text + " joined the game as a Client");
+        joinStatusTxt.SetText(NetworkManager.Singleton.LocalClientId + " " + playerNameInput.text + " joined the game as a Client");
     }
 
     public void JoinServer()
