f9077686cffffd06414897649603afcdfc4a5ba8
JackyWuCoder <JackyWuCoder@gmail.com>
2024-05-20 03:38:20 -0700

    TankColorChange.cs

--- Assets/Multiplayer Games Assets/Scripts/TankColorChange.cs
+++ Assets/Multiplayer Games Assets/Scripts/TankColorChange.cs
@@ -1,18 +1,62 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
+using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class TankColorChange : MonoBehaviour
+public class TankColorChange : NetworkBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private TMP_Dropdown colorDropdown;
+
+    // Network variable to synchronize color
+    // [SyncVar(hook = nameof(OnColorChanged))]
+    private Color tankColor = Color.white;
+
+    // Reference to the tank renderer
+    private Renderer tankRenderer;
+
+    private void Start()
     {
-        
+        tankRenderer = GetComponent<Renderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnColorChanged(Color newColor)
     {
-        
+        tankColor = newColor;
+        tankRenderer.material.color = newColor;
+    }
+
+    public void ChangeColor()
+    {
+        // Get the selected color from the dropdown
+        Color newColor = GetSelectedColor();
+
+        // Change the color locally and sync across the network
+        tankColor = newColor;
+        tankRenderer.material.color = newColor;
+
+        // If using UNET, CmdChangeColor method should be called to sync color across network
+    }
+
+    private Color GetSelectedColor()
+    {
+        // Get the selected color from the dropdown
+        string colorName = colorDropdown.options[colorDropdown.value].text;
+        return GetColorFromString(colorName);
+    }
+
+    private Color GetColorFromString(string colorName)
+    {
+        // Map color names to Color values
+        switch (colorName)
+        {
+            case "Red":
+                return Color.red;
+            case "Green":
+                return Color.green;
+            case "Blue":
+                return Color.blue;
+            default:
+                return Color.white;
+        }
     }
 }
